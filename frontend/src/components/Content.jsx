f1ccf603960a7d47a6ec07636393d7300037f1b0
David Quiros <david@quiros.online>
2025-04-17 21:45:45 -0600

    Touch up

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -4,12 +4,20 @@ function Content({ data, loading }) {
   return (
     <div className="content">
       {loading && !data ? (
-        <div>Fetching trending information...</div>
+        <div className="content-message">
+          <div className="content-loading">
+            <div className="content-loading-spinner"></div>
+            <p>Fetching trending information...</p>
+          </div>
+        </div>
       ) : data ? (
-        <div>{data}</div>
-      ) : (
         <div>
-          Select a category and click the button to see trending information.
+          <div className="content-data">{data}</div>
+        </div>
+      ) : (
+        <div className="content-message">
+          <p>Select a category and search.</p>
+          <p className="content-hint">See the trending information here.</p>
         </div>
       )}
     </div>
