ff4aa5e9792631c608223f272b304dd0b1b92da2
David Quiros <david@quiros.online>
2025-04-20 19:28:47 -0600

    Load env variables from .env file in dev mode only

--- backend/app/utils/setup.py
+++ backend/app/utils/setup.py
@@ -7,19 +7,20 @@ def setup():
     Set up the environment for the application.
     
     This function:
-    1. Loads environment variables from a .env file
-    2. Verifies the presence of required API keys
+    1. In development mode, loads environment variables from a .env file
+    2. In production mode, verifies the presence of required API keys
     3. Configures API keys in the environment
     
     Raises:
         SystemExit: If the .env file is not found or required environment variables are missing
     """
-    dotenv_path = find_dotenv(usecwd=True)
-    if not dotenv_path:
-        print("Error: .env file not found in the current directory or parent directories.")
-        sys.exit(1)
+    if os.getenv("ENV", "development").lower() == "development":
+      dotenv_path = find_dotenv(usecwd=True)
+      if not dotenv_path:
+          print("Error: .env file not found in the current directory or parent directories.")
+          sys.exit(1)
 
-    load_dotenv(dotenv_path)
+      load_dotenv(dotenv_path)
 
     openai_api_key = os.getenv("OPENAI_API_KEY")
     if not openai_api_key:
